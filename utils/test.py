ecba33efcd9f111b925e1df13826b588aff89158
Piseratopo <buinhatminh2k52005@gmail.com>
2026-01-28 23:55:23 +0700

    thêm lí thuyết về chuyển đổi từ dạng ngôn ngữ sang dạng kí hiệu

--- utils/test.py
+++ utils/test.py
@@ -3,5 +3,5 @@ from util_latex_general import parse_expression
 
 latex_output_file = "output.tex"
 
-write_logic_table_latex(latex_output_file, r"P +@ Q \/ R ~<=> (Q ~\/ R) & (P ~\/ ~R)")
+write_logic_table_latex(latex_output_file, r"(P => Q) ~<=> (Q => P)")
 
