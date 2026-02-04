b2aa9e0965291d8ff8d991b61ad19e5e90652ad6
Piseratopo <buinhatminh2k52005@gmail.com>
2026-02-04 23:58:36 +0700

    thêm 2 ví dụ về chứng minh

--- utils/test.py
+++ utils/test.py
@@ -3,5 +3,5 @@ from util_latex_general import parse_expression
 
 latex_output_file = "output.tex"
 
-write_logic_table_latex(latex_output_file, r"(P => Q) ~<=> (Q => P)")
+write_logic_table_latex(latex_output_file, r"(X => Y) & Y => X")
 
